d953ce4f4e4677f2979bbf659809b1590b9fd2b3
Chad Bates <cbates255@gmail.com>
2022-10-03 15:55:37 -0400

    coded

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,9 @@ output "vpc-id" {
 
 #the 'terraform output' command will show all saved outputs
 #for automation, use -json on the CLI to change return to json
+
+output "username" {
+  description = "DB username"
+  value = aws_db_instance.database.username
+  sensitive = true
+}
