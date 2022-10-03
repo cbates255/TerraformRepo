7fd0740217e5d09ac700553705452c1a16065ac1
Chad Bates <cbates255@gmail.com>
2022-10-03 15:08:56 -0400

    provisionerpractice

---
+++ provisioners.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "testinstance" {
+  ami = "aminame"
+  instance_type = "t2.micro"
+  key_name = "latestpair"
+  associate_public_ip_address = true
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install httpd && sudo systemctl start httpd"
+    ]
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("~/homeDesktop/AWS\ keypairs/latestpair.pem")
+      host = self.public_ip
+    }
+  }
+}
